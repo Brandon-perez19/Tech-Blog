20ed20e4584f6e815a8cfd4ed11ee105d018cc5d
Brandon <brandonperez@brandons-mbp.lan>
2022-07-08 12:24:09 -0400

    Set-up connection.js file.

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+import Sequelize, { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//creating connection to the database
+//setting up credentials with env access and heroku access
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize (process.env.JAWSDB_URL)
+: new Sequelize (process.env.JAWSDB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port:3306
+});
+
+export default sequelize
